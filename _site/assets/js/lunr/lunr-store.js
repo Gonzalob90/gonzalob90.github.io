1df4d619db290efb6613cd120dedd8eea37e8caa
Gonzalo <gonzalo.b90@gmail.com>
2020-01-30 19:59:49 +0000

    change in names

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Non-Synergistic Variational Auto-Encoders",
         "excerpt":"H1 heading H3 heading Basic text Our world is hierarchical and compositional, humans can generalise better since we use primitive concepts that allow us to create complex representations (Higgins et al. (2016)). Towards the creation of truly intelligent systems, they should learn in a similar way resulting in an increase...","categories": [],
         "tags": ["generative modeling","variational inference"],
-        "url": "http://localhost:4000/dissertation/",
+        "url": "http://localhost:4000/non-syn-vae/",
         "teaser":null}]
