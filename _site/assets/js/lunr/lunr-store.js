7793c826aa00d0d0261fa598da4236d195207fe8
Gonzalo <gonzalo.b90@gmail.com>
2020-02-03 09:38:57 +0000

    add text to post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Non-Synergistic Variational Auto-Encoders",
-        "excerpt":"Work presented at NeurIPS 2018, Workshop LatinX in AI Our world is hierarchical and compositional, humans can generalise better since we use primitive concepts that allow us to create complex representations (Higgins et al. (2016)). Towards the creation of truly intelligent systems, they should learn in a similar way resulting...","categories": [],
+        "excerpt":"Work presented at NeurIPS 2018, Workshop LatinX in AI Github (Reproduce results): Link Our world is hierarchical and compositional, humans can generalise better since we use primitive concepts that allow us to create complex representations (Higgins et al. (2016)). Towards the creation of truly intelligent systems, they should learn in...","categories": [],
         "tags": ["generative modeling","variational inference"],
         "url": "http://localhost:4000/non-syn-vae/",
         "teaser":null}]
